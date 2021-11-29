61ce4dc3cf8da9d4695759b2b5c12440335f9ad9
Julien Rousseau <julien.rousseau@apside-groupe.com>
2021-11-29 14:27:32 +0100

    Password requirements

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -18,29 +20,35 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Votre prénom',
+                'constraints' => new Length(false, 2, 30),
                 'attr' => ['placeholder' => 'Saisissez votre prénom']
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Votre nom',
+                'constraints' => new Length(false, 2, 30),
                 'attr' => ['placeholder' => 'Saisissez votre nom']
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Votre email',
+                'constraints' => new Length(false, 2, 60),
                 'attr' => ['placeholder' => 'Saisissez votre adresse email']
             ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Votre mot de passe',
-                'attr' => ['placeholder' => 'Saisissez votre mot de passe']
-            ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmez votre mot de passe',
-                'mapped' => false,
-                'attr' => ['placeholder' => 'Confirmez votre mot de passe']
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Mot de passe et confirmation doivent être identiques',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => ['placeholder' => 'Saisissez votre mot de passe']
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => ['placeholder' => 'Confirmez votre mot de passe']
+                ],
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Valider mon inscription'
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
