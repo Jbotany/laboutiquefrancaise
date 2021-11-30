7480b95ca8d98f10673a9a07d2f35b0fea6a4ce5
Julien Rousseau <julien.rousseau@apside-groupe.com>
2021-11-30 15:52:39 +0100

    Search functionnality

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Product;
+use App\Service\Search;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -17,6 +18,31 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * Permet de récupérer les produits en fonction de la recherche
+     *
+     * @param Search $search
+     * @return Product[]
+     */
+    public function findWithSearch(Search $search)
+    {
+        $query = $this->createQueryBuilder('p')
+            ->select('c', 'p')
+            ->join('p.category', 'c');
+
+        if (!empty($search->categories)) {
+            $query = $query->andWhere('c.id IN (:categories)')
+                ->setParameter('categories', $search->categories);
+        }
+
+        if (!empty($search->string)) {
+            $query = $query->andWhere('p.name LIKE :string')
+                ->setParameter('string', "%$search->string%");
+        }
+
+        return $query->getQuery()->getResult();
     }
 
     // /**
